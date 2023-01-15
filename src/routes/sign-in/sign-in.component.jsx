466389a1d4bce4055d4828a355ce24e76cb91fb3
Michael Dunlap <michaeldunlap@earthlink.net>
2023-01-14 19:26:16 -0900

    signup form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react'
+import { getRedirectResult } from '@firebase/auth'
+
+import { 
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
 
 const SignIn = () => {
+  useEffect(() => {
+    async function getRedirect() {
+      const res = await getRedirectResult(auth)
+      if (res) {
+        const userDocRef = await createUserDocumentFromAuth(res.user)
+      }
+    }
+    getRedirect()
+  }, [])
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
-    
   }
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
       </button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
+      <SignUpForm />
     </div>
   )
 }
